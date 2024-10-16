0062bc7a947eeffc0a684b1c64d1079d08c93566
AmerZakre <abouamro22@hotmail.com>
2024-10-16 09:49:36 +0200

    Initial commit

---
+++ src/main/java/Currensyconvert.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Currensyconvert {
+    public static void main(String[] args) {
+        Currensyconvert convert = new Currensyconvert();
+        Scanner scanner = new Scanner(System.in )
+         boolean isAdult= true;
+
+        int Currensyconvert = scanner.nextInt();
+
+        System.out.println("Currensyconvert menue" );
+        System.out.println( "1. convert dollar to euro");
+        System.out.println("2. convert sek to dollar");
+        System.out.println("3.convert sek to euro ");
+        System.out.println( "4. Exit");
+
+
+    }
+
+}
