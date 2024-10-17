ee1c1746115903cba068762440fbb289c4d13561
AmerZakre <abouamro22@hotmail.com>
2024-10-17 20:53:14 +0200

    Update Currensyconvert.java

--- src/main/java/Currensyconvert.java
+++ src/main/java/Currensyconvert.java
@@ -9,7 +9,6 @@
      public static void main(String[] args) {
 
          Currensyconvert convert = new Currensyconvert();
-         Scanner scanner = new Scanner(System.in )
          Scanner scanner = new Scanner(System.in );
           boolean isAdult= true;
  
@@ -17,14 +16,11 @@
          int Currensyconvert = scanner.nextInt();
  
          System.out.println("Currensyconvert menue" );
-       System.out.println( "1. convert dollar to euro");
-       System.out.println("2. convert sek to dollar");
-        System.out.println("3.convert sek to euro ");
-        System.out.println( "1. convert SEK to USD");
+       System.out.println( "1. convert SEK to USD");
         System.out.println("2. convert USD  to SEK");
        System.out.println("3.convert SEK to Euro ");
        System.out.println("4. convert Euro to SEK");
-         System.out.println( "4. Exit");
+         System.out.println( "5. Exit");
  
          int choice= scanner
 
